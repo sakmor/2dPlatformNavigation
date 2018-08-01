6f132eedb9fdc64e203df1a377e99f7e608bed77
sakmor <sakmor@gmail.com>
2018-08-01 14:49:04 +0800

    改改改

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -4,34 +4,44 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
-    [SerializeField] internal GameObject MoveCursor;
-    [SerializeField] private Player Player;
-    [SerializeField] internal Collider2D Ladder;
-    // Use this for initialization
-    void Start()
-    {
+	[SerializeField] internal GameObject MoveCursor;
+	[SerializeField] private Player Player;
+	[SerializeField] internal Collider2D Ladder, Ground;
+	// Use this for initialization
+	void Start()
+	{
 
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        InputUpdate();
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		InputUpdate();
+	}
 
-    void InputUpdate()
-    {
-        if (Input.GetMouseButtonDown(0) == false) return;
-        Vector3 mousePoint = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
-        if (Physics2D.Raycast(mousePoint, Vector2.zero)) return;
+	void InputUpdate()
+	{
+		if (Input.GetMouseButton(0) == false) return;
+		Debug.Log("GetMouseButtonDown");
 
-        RaycastHit2D hit = Physics2D.Raycast(mousePoint, Vector2.down, 1);
-        if (hit == false) return;
+		Vector3 mousePoint = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
+		if (Physics2D.Raycast(mousePoint, Vector2.zero, Mathf.Infinity, 1, -1000f, 1000f)) { Debug.Log("hitBlock"); return; }
 
-        Vector3 pos = hit.point;
-        if (Player.IsLadder) Player.MoveTo(pos, "Player@Back");
-        if (Mathf.Abs(pos.y - Player.transform.position.y) <= 1) Player.MoveTo(pos, "Player@Walk");
-        MoveCursor.transform.position = pos;
-        MoveCursor.SetActive(true);
-    }
+		RaycastHit2D hit = Physics2D.Raycast(mousePoint, Vector2.down, Mathf.Infinity, 1, -1000f, 1000f);
+		if (hit == false) { Debug.Log("Did not hit Block down "); return; }
+
+		Vector3 pos = hit.point;
+
+
+		MoveCursor.transform.position = pos;
+		MoveCursor.SetActive(true);
+		if (Mathf.Abs(pos.y - Player.transform.position.y) >= 1 && Player.IsLadder == false)
+		{
+			Player.ShowDialogICANT();
+			return;
+		}
+
+		Player.HideDialogICANT();
+		Player.MoveTo(pos, "Player@Walk");
+	}
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,58 +5,84 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private Main Main;
-    [SerializeField] private float speed = 0.05f;
-    [SerializeField] private Animator Animator;
-    [SerializeField] private SpriteRenderer Sprite;
-    [SerializeField] internal bool IsLadder;
-    [SerializeField] private Collider2D Collider2D;
+	[SerializeField] private Main Main;
+	[SerializeField] private float speed = 0.05f;
+	[SerializeField] private Animator Animator;
+	[SerializeField] private SpriteRenderer Sprite;
+	[SerializeField] internal bool IsLadder, IsGround;
+	[SerializeField] private Collider2D Collider2D;
+	[SerializeField] private GameObject DialogICANT, DialogLadder;
 
-    private Vector2 GoalPos;
+	private Vector2 GoalPos;
 
-    private void Start()
-    {
-        GoalPos = transform.position;
-    }
-    internal void MoveTo(Vector2 pos, string animation)
-    {
-        Animator.Play(animation);
-        GoalPos = pos;
-    }
-    private void Update()
-    {
-        Movement();
-    }
 
-    private void Movement()
-    {
-        if (Vector2.Distance(transform.position, GoalPos) < 0.01f) { Stop(); return; }
-        Vector3 CurrentPos = transform.position;
+	private void Start()
+	{
+		Stop();
+	}
+	internal void MoveTo(Vector2 pos, string animation)
+	{
+		Animator.Play(animation);
+		GoalPos = pos;
+	}
+	private void Update()
+	{
+		Movement();
+	}
 
-        Sprite.flipX = false;
-        if (CurrentPos.x > GoalPos.x) Sprite.flipX = true;
 
-        transform.position = Vector2.MoveTowards(CurrentPos, GoalPos, speed);
-        Collider2D.enabled = false;
-    }
+	private void Movement()
+	{
+		if (Vector2.Distance(transform.position, GoalPos) < 0.01f) { Stop(); return; }
+		Vector3 CurrentPos = transform.position;
 
-    private void Stop()
-    {
-        Animator.Play("Player@Idle");
-        Main.MoveCursor.SetActive(false);
-        Collider2D.enabled = true;
-    }
+		Sprite.flipX = false;
+		if (CurrentPos.x > GoalPos.x) Sprite.flipX = true;
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other != Main.Ladder) return;
-        IsLadder = true;
-    }
+		transform.position = Vector2.MoveTowards(CurrentPos, GoalPos, speed);
 
-    void OnTriggerExit2D(Collider2D other)
-    {
-        if (other != Main.Ladder) return;
-        IsLadder = false;
-    }
+		if (IsGround == false & IsLadder == false) Stop();
+
+	}
+
+	internal void Stop()
+	{
+		GoalPos = transform.position;
+		Animator.Play("Player@Idle");
+		Main.MoveCursor.SetActive(false);
+
+	}
+
+
+	internal void ShowDialogICANT()
+	{
+		DialogLadder.SetActive(false);
+		DialogICANT.SetActive(true);
+	}
+	internal void ShowDialogLadder()
+	{
+		DialogLadder.SetActive(true);
+		DialogICANT.SetActive(false);
+	}
+	internal void HideDialogICANT()
+	{
+		DialogICANT.SetActive(false);
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other == Main.Ladder) IsLadder = true;
+		if (other == Main.Ground) IsGround = true;
+	}
+	private void OnTriggerStay2D(Collider2D other)
+	{
+		if (other == Main.Ladder) ShowDialogLadder();
+		if (other == Main.Ground) IsGround = true;
+	}
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other == Main.Ladder) { IsLadder = false; DialogLadder.SetActive(false); }
+		if (other == Main.Ground) IsGround = false;
+	}
 
 }
